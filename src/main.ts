df65dee6ae0c1f3186ea2dd512370c9171f53705
Caius Citiriga <cc@kiboko.it>
2024-12-04 22:25:43 +0100

    --no-sandbox

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({
     headless: true,
+    args: ["--no-sandbox"],
   });
   const page = await browser.newPage();
 
